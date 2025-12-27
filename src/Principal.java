7d854ad96c53251760b627631b86acd36368b85d
nunesma <nunes.ma@ufs.br>
2025-12-27 15:16:51 -0300

    Atualizando o Principal

---
+++ src/Principal.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Iniciando o desafio");
+
+        var saldo = Math.random() * 2000;
+
+        saldo = Math.round(saldo * 100.0)/100.0;
+
+        System.out.println("O seu saldo atual e de R$ " + saldo + " reais");
+
+        System.out.println("Qual o valor para saque?");
+        var leitor = new Scanner(System.in);
+
+        var saque = leitor.nextDouble();
+
+        var motivo = "o saldo e insuficiente";
+
+        if (saque <= saldo && saque <= 1000.00 && saque >= 10.00 && (saque%10) == 0) {
+            System.out.println("Trasacao aprovada, voce pode sacar");
+        } else {
+            if (saque >= 1000.00 || saque < 10.00) {
+                motivo = "esta fora do limite maximo de R$ 1000,00 e minimo de R$ 10,00 reais";
+            } if ((saque%10) != 0) {
+                motivo = "temos apenas cedulas de R$ 10, R$ 20, R$ 50 e R$ 100 reais";
+            }
+            System.out.println("Saque nao autorizado porque " + motivo);
+        }
+    }
+}
